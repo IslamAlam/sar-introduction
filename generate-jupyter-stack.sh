031b0136a033af64074812a2751adf7cf4404e97
Islam Mansour <is3mansour@gmail.com>
2022-04-29 10:43:56 +0000

    build script

---
+++ generate-jupyter-stack.sh
@@ -0,0 +1,12 @@
+
+export DOCKER_ID_USER="imansour"
+export VERSION="0.0.14"
+
+docker build -t maap-sar-intro -t $DOCKER_ID_USER/maap-sar-intro:$VERSION -t $DOCKER_ID_USER/maap-sar-intro:latest .
+# docker tag maap-jupyterlab $DOCKER_ID_USER/maap-esa-jupyterlab:$VERSION
+docker push $DOCKER_ID_USER/maap-sar-intro:$VERSION
+docker push $DOCKER_ID_USER/maap-sar-intro:latest
+
+#to push in orange registry. 
+#docker tag maap-jupyterlab registry.eu-west-0.prod-cloud-ocb.orange-business.com/cloud-biomass-maap/maap-esa-jupyterlab:$VERSION
+#docker push registry.eu-west-0.prod-cloud-ocb.orange-business.com/cloud-biomass-maap/maap-esa-jupyterlab:$VERSION
